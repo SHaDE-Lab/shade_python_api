227b47aac5d7e7b4c0627a2266693810eaca025d
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2024-01-16 16:58:53 -0700

    setup daily jobs + schedule

--- solweig-generator.py
+++ solweig-generator.py
@@ -1,33 +1,36 @@
-import os
 import time
 from datetime import datetime, timedelta, timezone
 import pandas as pd
-from run_solweig_jobs import run_solweig_hourly, run_solweig_buildup
-
+from run_solweig_jobs import run_solweig_hourly, run_solweig_buildup, run_solweig_daily
+import schedule
 def startup_function(output_folder):
-    print("File generator started!")
-    os.makedirs(output_folder, exist_ok=True)
+    print("File generator started!", flush=True)
     run_solweig_buildup()
+    print("Finished startup function", flush=True)
 
-def generate_file(output_folder):
+def daily_function(output_folder):
+    print("Running daily function", flush=True)
+    run_solweig_daily(datetime.now(timezone.utc) + timedelta(days=3))
+
+def hourly_function(output_folder):
     hour_from_now = datetime.now(timezone.utc) + timedelta(hours=1)
     hour_from_now = pd.Timestamp(hour_from_now).replace(minute=0, second=0, microsecond=0)
     print(f"Generating file for {hour_from_now}")
     run_solweig_hourly(hour_from_now)
 
-def hourly_function(output_folder):
-    while True:
-        generate_file(output_folder)
-        # Sleep for one hour (3600 seconds)
-        time.sleep(3600)
-
 if __name__ == "__main__":
     # Specify the output folder (in the base directory)
-    print("Starting up solweig generator...")
+    print("Starting up solweig generator...", flush=True)
     output_folder = "output"
 
     # Call the startup function
     startup_function(output_folder)
 
-    # Run the hourly function
-    hourly_function(output_folder)
+    # Schedule the job to run every hour
+    schedule.every().hour.do(hourly_function(output_folder))
+    schedule.every().day.at("00:00").do((daily_function(output_folder)))
+    while True:
+        # Run pending jobs
+        print("Checking for jobs", flush=True)
+        schedule.run_pending()
+        time.sleep(60*60)
