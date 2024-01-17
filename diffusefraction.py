92700b5c3b9f44810c80384031a7cf689a4ef748
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2024-01-16 17:09:44 -0700

    add correction to diffusefraction

--- diffusefraction.py
+++ diffusefraction.py
@@ -11,6 +11,7 @@
 import numpy as np
 from sunpos import altitude
 
+
 def diffusefraction(radG, alt, Kt, Ta, RH):
     """
     This function e
@@ -20,40 +21,40 @@ def diffusefraction(radG, alt, Kt, Ta, RH):
     :param Ta:
     :param RH:
     :return:
-    
-    """
-    #lat = location['latitude']
-    #lon = location['longitude']
-	
-    #altitude = altitude(lat,lon,datetime)
 
-    alfa = alt*(np.pi/180)
+    """
+    # lat = location['latitude']
+    # lon = location['longitude']
+
+    # altitude = altitude(lat,lon,datetime)
+
+    alfa = alt * (np.pi / 180)
 
     if Ta <= -999.00 or RH <= -999.00 or np.isnan(Ta) or np.isnan(RH):
         if Kt <= 0.3:
-            radD = radG*(1.020-0.248*Kt)
+            radD = radG * (1.020 - 0.248 * Kt)
         elif Kt > 0.3 and Kt < 0.78:
-            radD = radG*(1.45-1.67*Kt)
+            radD = radG * (1.45 - 1.67 * Kt)
         else:
-            radD = radG*0.147
+            radD = radG * 0.147
     else:
-        RH = RH/100
+        RH = RH / 100
         if Kt <= 0.3:
-            radD = radG*(1 - 0.232 * Kt + 0.0239 * np.sin(alfa) - 0.000682 * Ta + 0.0195 * RH)
+            radD = radG * (1 - 0.232 * Kt + 0.0239 * np.sin(alfa) - 0.000682 * Ta + 0.0195 * RH)
         elif Kt > 0.3 and Kt < 0.78:
-            radD = radG*(1.329- 1.716 * Kt + 0.267 * np.sin(alfa) - 0.00357 * Ta + 0.106 * RH)
+            radD = radG * (1.329 - 1.716 * Kt + 0.267 * np.sin(alfa) - 0.00357 * Ta + 0.106 * RH)
         else:
-            radD = radG*(0.426 * Kt - 0.256 * np.sin(alfa) + 0.00349 * Ta + 0.0734 * RH)
+            radD = radG * (0.426 * Kt - 0.256 * np.sin(alfa) + 0.00349 * Ta + 0.0734 * RH)
 
-    radI = (radG - radD)/(np.sin(alfa))
+    radI = (radG - radD) / (np.sin(alfa))
 
     # Corrections for low sun altitudes (20130307)
+    if radD < 0:  # new addtion for Phoenix case
+        radD = 0
     if radI < 0:
         radI = 0
-
-    if alt < 1 and radI > radG:
-        radI=radG
-
+    if alt < 10 and radI > radG:  # originally if alt<1
+        radI = radG
     if radD > radG:
         radD = radG
 
