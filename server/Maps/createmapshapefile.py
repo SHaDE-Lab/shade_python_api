8cb623d70133093c5bea5989ba5c6b697263f041
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2024-02-13 14:25:34 -0700

    update coords

--- server/Maps/createmapshapefile.py
+++ server/Maps/createmapshapefile.py
@@ -1,41 +1,46 @@
 import osmnx as ox
 import geopandas as gpd
 # TODO CHANGE TO REALLY BE THE BOUNDING BOX
-north = 33.42796506379531
-west = -111.94015084151006
-
-south = 33.41592636331673
-east = -111.92634308211977
-granularity = 1
+North = 33.42943
+South = 33.409729
+East = -111.917768
+West = -111.941743
 
 # ox.settings.use_cache = True
-G = ox.graph_from_bbox(north, south, east, west)
+G = ox.graph_from_bbox(North, South, East, West, network_type='walk', simplify=True)
 
 ox.plot_graph(G)
 
+# saves graph as geopackage and shapefile
 ox.save_graph_geopackage(G, encoding='utf-8', filepath='downloadedShapeFile.gpkg')
+ox.save_graph_shapefile(G, filepath='downloadedShapeFile', encoding='utf-8')
 
-# # read in the downloadedgeopackage
-# load the shapefile
-nodes = gpd.read_file('downloadedShapeFile/nodes.shp')
-edges = gpd.read_file('downloadedShapeFile/edges.shp')
+def readFromShapeFile(node_filepath, edge_filepath):
+    # Read the GeoPackage file into GeoDataFrames
+    gdf_nodes = gpd.read_file(node_filepath, layer='nodes')
+    gdf_edges = gpd.read_file(edge_filepath, layer='edges')
 
-# Set the index of gdf_nodes to 'osmid'
-nodes.set_index('osmid', inplace=True)
+    # Set the index of gdf_nodes to 'osmid'
+    gdf_nodes.set_index('osmid', inplace=True)
 
-# Set the multi-index of gdf_edges to ['u', 'v', 'key']
-edges.set_index(['u', 'v', 'key'], inplace=True)
+    # Set the multi-index of gdf_edges to ['u', 'v', 'key']
+    gdf_edges.set_index(['u', 'v', 'key'], inplace=True)
+    # Convert the GeoDataFrames to a NetworkX graph
+    G = ox.graph_from_gdfs(gdf_nodes=gdf_nodes, gdf_edges=gdf_edges)
 
-# Specify the file path to the GeoPackage file
-file_path = 'downloadedShapeFile.gpkg'
+    return G
+def readFromGeopackage(file_path):
+    # Read the GeoPackage file into GeoDataFrames
+    gdf_nodes = gpd.read_file(file_path, layer='nodes')
+    gdf_edges = gpd.read_file(file_path, layer='edges')
 
-# Read the GeoPackage file into GeoDataFrames
-gdf_nodes = gpd.read_file(file_path, layer='nodes')
-gdf_edges = gpd.read_file(file_path, layer='edges')
+    # Set the index of gdf_nodes to 'osmid'
+    gdf_nodes.set_index('osmid', inplace=True)
 
+    # Set the multi-index of gdf_edges to ['u', 'v', 'key']
+    gdf_edges.set_index(['u', 'v', 'key'], inplace=True)
 
-# Convert the GeoDataFrames to a NetworkX graph
-G = ox.graph_from_gdfs(gdf_nodes=nodes, gdf_edges=edges)
+    # Convert the GeoDataFrames to a NetworkX graph
+    G = ox.graph_from_gdfs(gdf_nodes=gdf_nodes, gdf_edges=gdf_edges)
 
-
-ox.plot_graph(G)
+    return G
