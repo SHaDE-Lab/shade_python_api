a68e8b8991dc8db219f31c574c26ec31bd2db896
EvanCrabtree <75501111+EvanCrabtree@users.noreply.github.com>
2024-03-14 16:28:54 -0700

    add colon

--- run_solweig_jobs.py
+++ run_solweig_jobs.py
@@ -76,7 +76,7 @@ def file_cleanup():
         if file.endswith('.tif') or file.endswith('.graphml') and file < datetime.now().strftime('%Y-%m-%d-%H00'):
             print('deleting {0}'.format(file))
             # TODO DUMP TO LONG TERM STORAGE
-            if file != default_graph_path and file != default_mrt_file_path
+            if file != default_graph_path and file != default_mrt_file_path:
                 os.remove(os.path.join('output', file))
 
 
